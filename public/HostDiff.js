b8708769a95dd7b219241275ba279734f4ba4e3b
--replace-all <akshitaarora98@gmail.com>
2017-10-08 14:43:43 +0530

    Integrated diff module with codemirror

--- public/HostDiff.js
+++ public/HostDiff.js
@@ -1,12 +1,9 @@
-var host_input = document.getElementById('host_input');
-
 var prev = "";
 var cur = "";
 
-function change_occured() {
-    cur = host_input.value;
+function change_occured(data) {
+    cur = data;
     var diffArray = JsDiff["diffChars"](prev, cur);
     prev = cur;
     console.dir(diffArray);
 }
-host_input.onkeyup = change_occured;
