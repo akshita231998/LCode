2b07f787d90c2be26b2068d7e429b49869127a37
--replace-all <akshitaarora98@gmail.com>
2017-10-08 15:25:50 +0530

    Integrated diff-match-patch to codemirror

--- public/HostDiff.js
+++ public/HostDiff.js
@@ -1,9 +1,15 @@
+var dmp = new diff_match_patch();
 var prev = "";
 var cur = "";
 
-function change_occured(data) {
-    cur = data;
-    var diffArray = JsDiff["diffChars"](prev, cur);
-    prev = cur;
-    console.dir(diffArray);
+function change_occured(data){
+  cur = data;
+  var diff = dmp.diff_main(prev,cur);
+  if (diff.length > 2) {
+    dmp.diff_cleanupSemantic(diff);
+  }
+  var patch_list = dmp.patch_make(prev, cur, diff);
+  var res = dmp.patch_apply(patch_list, prev);
+  console.log(res[1]);
+  prev = cur;
 }
