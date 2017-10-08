00ec3d3d50581ac96d815a50462534eb6b13a85b
--replace-all <akshitaarora98@gmail.com>
2017-10-08 22:11:40 +0530

    Added patch functionality on server side

--- public/HostDiff.js
+++ public/HostDiff.js
@@ -2,6 +2,16 @@ var dmp = new diff_match_patch();
 var prev = "";
 var cur = "";
 
+var socket = io("http://localhost:3000");
+
+socket.on("connect",function() {
+  console.log("Socket connected");
+});
+
+socket.on("disconnect", function() {
+    console.log("Disconnected");
+});
+
 function change_occured(data){
   cur = data;
   var diff = dmp.diff_main(prev,cur);
@@ -9,7 +19,6 @@ function change_occured(data){
     dmp.diff_cleanupSemantic(diff);
   }
   var patch_list = dmp.patch_make(prev, cur, diff);
-  var res = dmp.patch_apply(patch_list, prev);
-  console.log(res[1]);
   prev = cur;
+  socket.emit("patch",patch_list);
 }
--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 var express = require("express");
+var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
+var server = http.createServer(app).listen(3000);
+var io = require("socket.io")(server);
 var unique_code = -1;
+var prev = "";
+var DiffMatchPatch = require('diff-match-patch');
+var dmp = new DiffMatchPatch();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use(function(req, res, next){
-	console.log(`${req.method} requested for ${req.url}`);
-	next();
-});
+app.use(express.static("./node_modules"));
 app.use(express.static("./public"));
-app.use(function(req, res, next){
-		console.log(`${req.method} ---- ${JSON.stringify(req.body)}`);
-		next();
+
+io.on("connection", function(socket)
+{
+	console.log("Hello");
+	socket.on("patch",function(patch_list) {
+		var res = dmp.patch_apply(patch_list, prev);
+		console.log(res[0]);
+		prev = res[0];
+	});
 });
-app.post("/api/unique_code",function(req,res) {
-	unique_code = req.body.code;
-});
-app.post("/api/validate_code",function(req,res) {
-	var user_input = req.body.code;
-	if(user_input == unique_code) {
-		res.send("True");
-	} else {
-		res.send("False");
-	}
-});
-app.listen(3000);
-module.exports = app;
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
+//
+// app.use(function(req, res, next){
+// 	console.log(`${req.method} requested for ${req.url}`);
+// 	next();
+// });
+//
+// app.use(function(req, res, next){
+// 		console.log(`${req.method} ---- ${JSON.stringify(req.body)}`);
+// 		next();
+// });
+//
+// app.post("/api/unique_code",function(req,res) {
+// 	unique_code = req.body.code;
+// });
+//
+// app.post("/api/validate_code",function(req,res) {
+// 	var user_input = req.body.code;
+// 	if(unique_code!=-1 && user_input == unique_code) {
+// 		res.send("True");
+// 	} else {
+// 		res.send("False");
+// 	}
+// });
+
+console.log("Server running on port 3000");
